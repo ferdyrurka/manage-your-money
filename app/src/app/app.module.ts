c400d7751044dd2797d6fa5e0379a8a08b7bd687
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2021-03-20 20:43:56 +0100

    init app

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {OperationModule} from './operation/operation.module';
+import {OperationRoutingModule} from './operation/operation-routing.module';
+import {environment} from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OperationModule,
+    OperationRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
