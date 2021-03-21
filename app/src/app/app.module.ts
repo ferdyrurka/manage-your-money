c56ba0491b79abbff99d1f8f1fe67caf498de053
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2021-03-21 13:21:46 +0100

    basic layout

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,18 +7,32 @@ import {OperationModule} from './operation/operation.module';
 import {OperationRoutingModule} from './operation/operation-routing.module';
 import {environment} from '../environments/environment';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTreeModule} from '@angular/material/tree';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserModule,
-    AppRoutingModule,
-    OperationModule,
-    OperationRoutingModule,
-  ],
+    imports: [
+        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+        BrowserModule,
+        AppRoutingModule,
+        OperationModule,
+        OperationRoutingModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        BrowserAnimationsModule,
+        MatListModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- app/src/app/operation/operation-routing.module.ts
+++ app/src/app/operation/operation-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent as TypeCreateComponent } from './page/type/create/create.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'operation',
+    children: [
+      {
+        path: 'type',
+        children: [
+          { path: 'create', component: TypeCreateComponent }
+        ]
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
