dcf7488a48c2c757bbbabf60cc4a8c6e79c7e22c
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2021-04-09 21:50:06 +0200

    fix for paginator

--- admin-app/src/app/shared/paginator/page-size-options.ts
+++ admin-app/src/app/shared/paginator/page-size-options.ts
@@ -1,7 +1,7 @@
 export class PageSizeOptions {
   public static readonly BASE = [5, 10, 25, 50, 100];
 
-  public static readonly MIN_COUNT = 5;
+  public static readonly MIN_COUNT = 6;
 
   public static getSizeOptions(totalCount: number): number[]
   {
